14afcff663b4f4cd14d02a90197bce00d5e0de42
zgiltner <zgiltner@gmail.com>
2022-12-03 02:37:52 -0500

    day 3 solutions

--- d3.py
+++ d3.py
@@ -1,45 +1,40 @@
 with open("d3input.txt") as f:
     data = f.read().splitlines()
-    pritotal = 0
+    priTotal = 0
 
 # part 1 find total of priority
 for line in data:
     half = len(line) // 2
-    firsthalf = (line[:-half])
-    secondhalf = (line[half:])
-    itemtype = []
-    #find intersection of first half and second half
-    itemtype.extend(set(firsthalf).intersection(secondhalf))
-    #make the set into a single letter
-    letter = itemtype[0]
-    #goofy ord() stuff
+    firstHalf = (line[:-half])
+    secondHalf = (line[half:])
+    itemType = []
+# find intersection of first half and second half
+    itemType.extend(set(firstHalf).intersection(secondHalf))
+# make the set into a single letter
+    letter = itemType[0]
+# goofy ord() stuff
     if ord(letter) < 91:
-        pritotal += (ord(letter)-38)
+        priTotal += (ord(letter)-38)
     else:
-        pritotal += (ord(letter)-96)
+        priTotal += (ord(letter)-96)
 
-#part 1 solution
-print(pritotal)
+# part 1 solution
+print(priTotal)
 
 # find intersection of every 3 lines
-badgetotal = 0
+badgeTotal = 0
 
 for line in range(0, len(data), 3):
-    firstline = data[line]
-    secondline = data[line+1]
-    thirdline = data[line+2]
-    groupbadges = []
-    groupbadges.extend(set(firstline).intersection(secondline, thirdline))
-    badge = groupbadges[0]
+    firstLine = data[line]
+    secondLine = data[line+1]
+    thirdLine = data[line+2]
+    groupBadges = []
+    groupBadges.extend(set(firstLine).intersection(secondLine, thirdLine))
+    badge = groupBadges[0]
     if ord(badge) < 91:
-        badgetotal += (ord(badge)-38)
+        badgeTotal += (ord(badge)-38)
     else:
-        badgetotal += (ord(badge)-96)
+        badgeTotal += (ord(badge)-96)
 
-#part 2 solution
-print(badgetotal)
-
-
-
-
-
+# part 2 solution
+print(badgeTotal)
